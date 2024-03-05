71d2c2aa784a63a6190514fdeac3e10ef082a7c6
Alain <yinxulai@hotmail.com>
2024-03-05 23:08:40 +0800

    v2 in WIP

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yinxulai/http-agent
+
+go 1.21.0
--- main.go
+++ main.go
@@ -1,111 +1,165 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"net/url"
+	"strings"
 )
 
-type request struct {
-	Url    string              `json:"url"`
-	Method string              `json:"method"`
-	Body   *[]byte             `json:"body"`
-	Header map[string][]string `json:"header"`
+const (
+	// Use in Response
+	OverwriteRequestUrl           = "http-agent-overwrite-request-url"      // example: http-agent-overwrite-request-url: https://example.com
+	ExposeResponseHeader          = "http-agent-expose-response-header"     // example: http-agent-expose-response-header: set-cookie
+	OverwriteRequestHeaderPrefix  = "http-agent-overwrite-request-header-"  // example: http-agent-overwrite-request-header:cookie: example-cookie=example
+	OverwriteResponseHeaderPrefix = "http-agent-overwrite-response-header-" // example: http-agent-overwrite-response-header:Access-Control-Allow-Origin: *
+
+	// Use in Response
+	ExposedResponseHeader = "http-agent-exposed-response-header-" // 用于在 response 中暴露 ExposeResponseHeader 指定的 header
+)
+
+type AgentOptions struct {
+	OverwriteRequestURL     string
+	ExposeResponseHeader    []string
+	OverwriteRequestHeader  map[string][]string
+	OverwriteResponseHeader map[string][]string
 }
 
-type response struct {
-	Status int                 `json:"status"`
-	Body   *[]byte             `json:"body"`
-	Header map[string][]string `json:"header"`
+func ParseOptionsFormHeader(header *http.Header) *AgentOptions {
+	var agentOptions AgentOptions
+
+	var supportedOptionPrefixList = []string{
+		OverwriteRequestUrl,
+		ExposeResponseHeader,
+		OverwriteRequestHeaderPrefix,
+		OverwriteResponseHeaderPrefix,
+	}
+
+	for headerKey, headerValues := range *header {
+		lowerHeaderKey := strings.ToLower(headerKey)
+		for _, supportedOptionPrefix := range supportedOptionPrefixList {
+			if strings.HasPrefix(lowerHeaderKey, supportedOptionPrefix) {
+				// OverwriteRequestURL
+				if supportedOptionPrefix == OverwriteRequestUrl {
+					agentOptions.OverwriteRequestURL = headerValues[0]
+				}
+
+				// ExposeResponseHeader
+				if supportedOptionPrefix == ExposeResponseHeader {
+					agentOptions.ExposeResponseHeader = headerValues
+				}
+
+				// OverwriteRequestHeader
+				if supportedOptionPrefix == OverwriteRequestHeaderPrefix {
+					if (agentOptions.OverwriteRequestHeader == nil) {
+						agentOptions.OverwriteRequestHeader = make(map[string][]string)
+					}
+
+					overwriteKey, _ := strings.CutPrefix(lowerHeaderKey, OverwriteRequestHeaderPrefix)
+					agentOptions.OverwriteRequestHeader[overwriteKey] = headerValues
+				}
+
+				// OverwriteResponseHeader
+				if supportedOptionPrefix == OverwriteResponseHeaderPrefix {
+					if (agentOptions.OverwriteResponseHeader == nil) {
+						agentOptions.OverwriteResponseHeader = make(map[string][]string)
+					}
+
+					overwriteKey, _ := strings.CutPrefix(lowerHeaderKey, OverwriteResponseHeaderPrefix)
+					agentOptions.OverwriteResponseHeader[overwriteKey] = headerValues
+				}
+
+				// 清理请求的 header 便于后面使用
+				header.Del(lowerHeaderKey)
+			}
+		}
+	}
+
+	return &agentOptions
 }
 
-func requestHandler(w http.ResponseWriter, r *http.Request) {
+func AutoProxy(w http.ResponseWriter, request *http.Request) {
 	// 设置允许跨域访问的源
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "*")
 
 	// 跨域预检请求
-	if r.Method == "OPTIONS" {
+	if request.Method == "OPTIONS" {
 		return
 	}
 
-	if r.Method != "POST" {
-		err := fmt.Errorf("Non-POST request received")
+	agentOptions := ParseOptionsFormHeader(&request.Header)
+	log.Printf("Received a request: %v", agentOptions)
+
+	// 处理 OverwriteRequestURL
+	if agentOptions.OverwriteRequestURL == "" {
+		err := fmt.Errorf("OverwriteRequestURL cannot be empty")
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	var requestParams request
-	err := json.NewDecoder(r.Body).Decode(&requestParams)
+	agentRequest, err := http.NewRequestWithContext(request.Context(), request.Method, agentOptions.OverwriteRequestURL, request.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		log.Fatal(err)
 		return
 	}
 
-	log.Printf("Received a request: %s %s", requestParams.Method, requestParams.Url)
-
-	requestParamUrl, err := url.Parse(requestParams.Url)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		log.Fatal(err)
-		return
+	// 转发全部的 header
+	for requestHeaderKey, requestHeaderValues := range request.Header {
+		for _, requestHeaderValue := range requestHeaderValues {
+			agentRequest.Header.Add(requestHeaderKey, requestHeaderValue)
+		}
 	}
 
-	targetRequest := &http.Request{}
-	targetRequest.URL = requestParamUrl
-	targetRequest.Method = requestParams.Method
-	targetRequest.Header = requestParams.Header
-	if requestParams.Body != nil {
-		readerCloser := io.NopCloser(bytes.NewReader(*requestParams.Body))
-		targetRequest.Body = readerCloser
+	// 处理 OverwriteRequestHeader
+	for overwriteKey, overwriteValues := range agentOptions.OverwriteRequestHeader {
+		agentRequest.Header.Del(overwriteKey)
+		for _, overwriteValue := range overwriteValues {
+			agentRequest.Header.Add(overwriteKey, overwriteValue)
+		}
 	}
 
+	// 发送请求
 	client := &http.Client{}
-	targetResponse, err := client.Do(targetRequest)
+	agentResponse, err := client.Do(agentRequest)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		log.Fatal(err)
 		return
 	}
 
-	var responseResult response
-	responseResult.Header = targetResponse.Header
-	responseResult.Status = targetResponse.StatusCode
-
-	bytes, err := io.ReadAll(targetResponse.Body)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		log.Fatal(err)
-		return
-	}
-	responseResult.Body = &bytes
-
-	responseResultBytes, err := json.Marshal(responseResult)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		log.Fatal(err)
-		return
+	// 处理 ExposeResponseHeader
+	for _, exposeKey := range agentOptions.ExposeResponseHeader {
+		exposedKey := ExposedResponseHeader + exposeKey
+		exposedValues := agentResponse.Header.Values(exposeKey)
+		for _, exposedValue := range exposedValues {
+			agentResponse.Header.Add(exposedKey, exposedValue)
+		}
 	}
 
-	log.Printf("Processed a request: %s %s", requestParams.Method, requestParams.Url)
-	if _, err := w.Write(responseResultBytes); err != nil {
-		log.Println("Failed to write response:", err)
+	// 处理 OverwriteResponseHeader
+	for overwriteKey, overwriteValues := range agentOptions.OverwriteResponseHeader {
+		agentResponse.Header.Del(overwriteKey)
+		for _, overwriteValue := range overwriteValues {
+			agentResponse.Header.Add(overwriteKey, overwriteValue)
+		}
+	}
+
+	// 将响应结果返回到 response
+	if err := agentResponse.Write(w); err != nil {
+		log.Fatal("Write response fatal:", err)
 	}
 }
 
 func main() {
 	// 注册请求处理函数
-	http.HandleFunc("/", requestHandler)
+	http.HandleFunc("/", AutoProxy)
 
 	// 启动 HTTP 服务，监听在端口808
-	log.Println("start the server on port 80")
-	err := http.ListenAndServe(":80", nil)
+	log.Println("start the server on port 8080")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("Failed to start the server:", err)
 	}
